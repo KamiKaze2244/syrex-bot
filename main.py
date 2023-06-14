34e5f32faf53f50fb97e0ab8574153df63094afc
KamiKaze2244 <135472761+KamiKaze2244@users.noreply.github.com>
2023-06-15 00:28:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -81,13 +81,13 @@ async def sustur(ctx, member: discord.Member):
 @Bot.event
 async def on_member_join(member):
     channel = Bot.get_channel(1118331806014505072)  # Hedeflenen oda ID'sini buraya girin
-    if channel is not None: #burası olmasada çalışır ama terminalde hata verir program durur
+    if channel is not None: #burası olmasada çalışır eğer channel id girilmediyse terminalde hata verir program durur
         await channel.send(f"Merhaba {member.mention}, hoş geldin!")
 
 @Bot.event
 async def on_member_remove(member):
     channel = Bot.get_channel(1118331806014505072)
-    if channel is not None: #burası olmasada çalışır ama terminalde hata verir program durur
+    if channel is not None: #burası olmasada çalışır eğer channel id girilmediyse terminalde hata verir program durur
         await channel.send(f"Görüşürüz {member.name}, hoşça kal!")
         
       
